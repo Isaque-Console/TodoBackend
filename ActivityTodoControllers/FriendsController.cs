ea4ee57f2d1ee60c900a5bf9d44e840cd3e21910
Isaque Araújo Nogueira <isaquean2@gmail.com>
2020-09-03 11:59:22 -0300

    Project todo backend

---
+++ ActivityTodoControllers/FriendsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Todo.Example.Api.Entities;
+using Todo.Example.Api.Repository;
+
+namespace Todo.Example.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/activity")]
+    public class ActivityController : ControllerBase
+    {
+        public RepositoryFile _repositoryFile;
+
+        public ActivityController() =>
+            this._repositoryFile = new RepositoryFile();
+
+        [HttpGet]
+        public IEnumerable<Activity> GetAll()
+        {
+            return _repositoryFile.GetAllTodoFromFile();
+        }
+
+        [HttpGet("{id}")]
+        public Activity GetById(int id)
+        {
+            return _repositoryFile.GetTodoById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Activity activity)
+        {
+            _repositoryFile.Insert(activity);
+        }
+    }
+}
---
+++ Repository/RepositoryFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Todo.Example.Api.Entities;
+
+namespace Todo.Example.Api.Repository
+{
+    public class RepositoryFile
+    {
+        readonly private string[] _lines;
+
+        public RepositoryFile() =>
+            this._lines = File.ReadAllLines("..\\Todo.Example.Api\\FileDatabase\\db.txt");
+
+        public List<Activity> GetAllTodoFromFile()
+        {
+            List<Activity> todos = new List<Activity>();
+
+            foreach (var line in _lines)
+            {
+                var data = line.Split(',');
+                var activity = new Activity(
+                                    long.Parse(data[0]),
+                                    data[1],
+                                    data[2],
+                                    data[3],
+                                    data[4],
+                                    data[5]);
+
+                todos.Add(activity);
+            }
+
+            return todos;
+        }
+
+        public Activity GetTodoById(int id)
+        {
+            var result = _lines.ElementAt(id);
+            var data = result.Split(',');
+
+            return new Activity(long.Parse(data[0]), data[1], data[2], data[3], data[4], data[5]);
+        }
+
+        public void Insert(Activity activity)
+        {
+            string[] line = new string[] { $"{Environment.NewLine}{activity.Id}, {activity.Name}, {activity.Description}, {activity.DateStart}, {activity.DateFinished}, {activity.Status}" };
+            File.AppendAllLines("..\\Todo.Example.Api\\FileDatabase\\db.txt", line);
+        }
+    }
+}
