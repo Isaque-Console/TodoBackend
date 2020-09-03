30bb4cb46ecd0841322a5163f29b2d3e2ade46e2
Isaque Araújo Nogueira <isaquean2@gmail.com>
2020-09-03 11:52:07 -0300

    First commit

---
+++ Activity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Example.Api.Entities
+{
+    public class Activity : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string DateStart { get; private set; }
+        public string DateFinished { get; private set; }
+        public string Status { get; private set; }
+
+        public Activity() { }
+        public Activity(long id, string name, string descr, string datastart, string datafinished, string status)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = descr;
+            this.DateStart = datastart;
+            this.DateFinished = datafinished;
+            this.Status = status;
+        }
+    }
+    
+}
---
+++ Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Example.Api.Entities
+{
+    public class Entity
+    {
+        public long Id { get; set; } 
+    }
+}
